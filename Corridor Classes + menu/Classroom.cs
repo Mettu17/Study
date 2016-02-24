f9d5fada833f3bc9808decb23f1cf51635947a66
Matthew <matthew.muscat1@gmail.com>
2016-02-24 10:00:58 +0100

    Classrooms

---
+++ Corridor Classes + menu/Classroom.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corridor_Classes___menu
+{
+    class Classroom
+    {
+
+        public string classroomname;
+        public bool projectoravailable;
+        int computersinclassroom;
+
+        public string Classroomname
+        {
+            get
+            {
+                return classroomname;
+            }
+
+            set
+            {
+                classroomname = value;
+            }
+        }
+
+        public bool Projectoravailable
+        {
+            get
+            {
+                return projectoravailable;
+            }
+
+            set
+            {
+                projectoravailable = value;
+            }
+        }
+
+
+        Computer[] conmputersinclassrom;
+        internal Computer[] Conmputersinclassrom
+        {
+            get
+            {
+                return conmputersinclassrom;
+            }
+
+            set
+            {
+                conmputersinclassrom = value;
+            }
+        }
+
+        
+
+        public Classroom(String classroom, bool projectoravailable,Computer[] allcomputers)    
+            {
+                Classroomname = classroomname;
+                Projectoravailable = projectoravailable;
+                computersinclassroom = allcomputers; 
+
+            }
+                
+        public  string outputclassroom()
+        {
+            return "Name Of Classroom:" + classroomname + "Projector:" + projectoravailable;
+
+        }
+
+
+        public override string ToString()
+        {
+            String output = "";
+            output += "Name of Classroom: " + Classroomname + " Projector: " + Projectoravailable;
+            foreach(Computer c in Computerinclassroom)
+            {
+                output += c;
+
+            }
+            return output;
+        }
+
+
+
+    }
+
+
+}
---
+++ Corridor Classes + menu/Computer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corridor_Classes___menu
+{
+    class Computer
+    {
+        private string computername;
+        private string computerstatus;
+        private int numberoffaults;
+
+        public string Computername
+        {
+            get
+            {
+                return computername;
+            }
+
+            set
+            {
+                computername = value;
+            }
+        }
+
+        public string Computerstatus
+        {
+            get
+            {
+                return computerstatus;
+            }
+
+            set
+            {
+                computerstatus = value;
+            }
+        }
+
+        int Numberoffaults
+        {
+            get
+            {
+                return numberoffaults;
+            }
+
+            set
+            {
+                numberoffaults = value;
+            }
+        }
+
+        public Computer(String cname, String status, int faults)
+        {
+             Computername = cname;
+             Computerstatus = status;
+             Numberoffaults = faults;
+        }
+
+
+        public int showNumberOfFaults()
+        {
+            return Numberoffaults;
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            output = "===========================================\n";
+            output += "Computer Name: " + Computername + "\n";
+            output += "Computer Status: " + Computerstatus + "\n";
+            output += "Number of Faults: " + Numberoffaults + "\n";
+            output += "==========================================\n";
+
+            return output;
+                
+        }
+
+
+
+
+    }
+
+
+}
